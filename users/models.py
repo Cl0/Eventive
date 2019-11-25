6de531ea7c8180cf1746e60bccbf5c50488be4e9
Cl0 <shawchloej@hotmail.com>
2019-11-25 14:18:37 +0800

    adding additional styles to all pages, images to event pages and registration form

--- users/models.py
+++ users/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
   '''events = models.OneToMany(Event)'''
+  firstname = models.CharField(max_length=50)
+  surname = models.CharField(max_length=50)
+     
   # add additional fields in here
 
   def __str__(self):
